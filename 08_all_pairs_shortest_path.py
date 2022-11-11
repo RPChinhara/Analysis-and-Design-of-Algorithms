64907a80bc3ffbac40188debbd48a10c3a2c0a5a
RPChinhara <rpchinhara@gmail.com>
2022-11-11 12:01:53 +0530

    all pairs shortest path

--- 08_all_pairs_shortest_path.py
+++ 08_all_pairs_shortest_path.py
@@ -2,23 +2,12 @@ import sys
 
 INF = sys.maxsize
 
-def shortest_path(V, E, edges):
+def shortest_path(costMat, V):
     graph = [[INF for i in range(V)] for j in range(V)]
-
-    for i in range(E):
-        # Get source and destination
-        # with weight
-        u = edges[i][0]
-        v = edges[i][1]
-        w = edges[i][2]
-
-        # Add the edges
-        graph[u][v] = w
-        graph[v][u] = w
 
     for i in range(V):
         for j in range(V):
-            graph[i][j] = graph[i][j]
+            graph[i][j] = costMat[i][j]
 
     for k in range(V):
         for i in range(V):
@@ -26,20 +15,31 @@ def shortest_path(V, E, edges):
                 if graph[i][k] != INF and graph[k][j] != INF:
                     graph[i][j] = min(graph[i][j], graph[i][k] + graph[k][j])
 
-    sum = 0
-
-    for i in range(V):
-        for j in range(i + 1, V):
-            sum += graph[i][j]
-
-    return sum
+    return graph
 
 
 # Number of Vertices
-V = 4
-# Number of Edges
-E = 3
-# Given Edges with weight
-Edges = [[0, 1, 1], [1, 2, 2], [2, 3, 3]]
+V = 7
 
-print(shortest_path(V, E, Edges))
+# Cost Matrix
+cost_matrix = [ [0, 3, 6, INF, INF, INF, INF],
+                [3, 0, 2, 1, INF, INF, INF],
+                [6, 2, 0, 1, 4, 2, INF],
+                [INF, 1, 1, 0, 2, INF, 4],
+                [INF, INF, 4, 2, 0, 2, 1],
+                [INF, INF, 2, INF, 2, 0, 1],
+                [INF, INF, INF, 4, 1, 1, 0]]
+
+
+output = shortest_path(cost_matrix, V)
+
+print("Output Matrix:")
+for i in range(V):
+    print(output[i])
+
+sum = 0
+for i in range(V):
+    for j in range(i + 1, V):
+        sum += output[i][j]
+
+print("Sum of shortest path is: ", sum)

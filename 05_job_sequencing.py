4e930a64b3a1d1198c4126298ff7d0f04e312892
RPChinhara <rpchinhara@gmail.com>
2022-12-03 10:13:07 +0530

    05_job_sequencing

--- 05_job_sequencing.py
+++ 05_job_sequencing.py
@@ -17,7 +17,7 @@ n = int(input("Enter number of jobs: "))
 jobs = []
 
 for i in range(n):
-    job_id = input("Enter job id: ")
+    job_id = int(input("Enter job id: "))
     arr = input("Enter the profit and deadline: ").split(" ")
     arr = [int(i) for i in arr]
     arr.insert(0, job_id)

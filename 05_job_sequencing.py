86586fcbb2fa87720f62443eff162a2e72b07ea2
RPChinhara <rpchinhara@gmail.com>
2022-12-04 18:32:33 +0530

    05_job_sequencing

--- 05_job_sequencing.py
+++ 05_job_sequencing.py
@@ -1,16 +1,15 @@
 def job_sequencing(arr):
     arr.sort(key = lambda i:i[2], reverse=True)
     max_deadline = arr[0][2]
-    
     job_execution = []
-    
+
     for i in range(max_deadline):
         max_item = arr[0]
         for item in arr:
             if item[2] == i + 1 and item[1] > max_item[1]:
                 max_item = item
         job_execution.append((max_item[0], max_item[1]))
-    
+
     return job_execution
 
 n = int(input("Enter number of jobs: "))

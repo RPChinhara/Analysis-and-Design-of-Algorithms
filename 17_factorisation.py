af68f9d81337a751c73d946b7ad367fae356dfae
RPChinhara <rpchinhara@gmail.com>
2023-01-21 18:08:55 +0530

    integer factorisation

---
+++ 17_factorisation.py
@@ -0,0 +1,10 @@
+def factorisation(n):
+    fact = []
+    i = 2
+    while i<=n:     
+        if n%i==0:      
+            fact.append(i)
+            n//= i
+        else:
+            i+=1
+    return fact
